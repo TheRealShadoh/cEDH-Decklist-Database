e72c7267cb36326a5c3465714bf79bae0a89833e
Christian Crill <chcrill@microsoft.com>
2020-02-19 05:01:13 -0600

    Using a file to fix the card name issue... temporary fix

--- posh/Get-Decklist.ps1
+++ posh/Get-Decklist.ps1
@@ -3,7 +3,26 @@ Import-module $PSScriptRoot/functions.psm1
 
 #$testurl = 'https://tappedout.net/mtg-decks/grenzo-ft-dj-doomsday-competitive-primer/'
 
-$test = decklistGet -url $testurl
-$test.defaultValue | out-file .\tmpOut.txt
-$cleanedData = Get-Content .\tmpOut.txt
-Remove-item .\tmpOut.txt
+$dbPrimary = 'https://sheets.googleapis.com/v4/spreadsheets/1NYZ2g0ETfGulhPKYAKrKTPjviaLERKuvyKyk9oizV8Q/values/!A2:J?key=AIzaSyCy2pE5znDZ9uDdpSgYb2Q992r0YOIPuIw'
+$dbFringe = "https://sheets.googleapis.com/v4/spreadsheets/1NYZ2g0ETfGulhPKYAKrKTPjviaLERKuvyKyk9oizV8Q/values/fringe!A2:J?key=AIzaSyCy2pE5znDZ9uDdpSgYb2Q992r0YOIPuIw"
+
+$primaryData = dbGet -url $dbPrimary
+$fringeData = dbGet -url $dbFringe
+
+$primaryLists = dbParse -DataInput $primaryData
+$fringeLists = dbParse -DataInput $fringeData
+
+$combinedLists = @()
+$combinedLists += $primaryLists
+$combinedLists += $fringeLists
+
+
+$array = @()
+Foreach ($list in $combinedLists.list)
+{
+        $deckList = decklistGet -url $list
+        $deckList.defaultValue | out-file .\tmpOut.txt
+        $cleanedData = Get-Content .\tmpOut.txt
+        Remove-item .\tmpOut.txt
+        $array += $cleanedData
+}
